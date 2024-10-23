45aa0b2d1a0d98b444fab30b49b5311a803e46ea
ST10145185 <ST10145185@vcconnect.edu.za>
2024-10-23 15:38:48 +0200

    Log a task feature

--- app/src/main/java/com/example/xbcad7319/CreateTask.kt
+++ app/src/main/java/com/example/xbcad7319/CreateTask.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class CreateTask : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_create_task)
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         // Initialize Firestore and Firebase Authentication
         db = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
