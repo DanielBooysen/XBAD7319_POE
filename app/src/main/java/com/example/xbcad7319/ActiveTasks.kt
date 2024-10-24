b12ac3c4c36787e95dd1af61db7cb59b73f44a77
ST10145185 <ST10145185@vcconnect.edu.za>
2024-10-24 13:45:23 +0200

    Bug fix

--- app/src/main/java/com/example/xbcad7319/ActiveTasks.kt
+++ app/src/main/java/com/example/xbcad7319/ActiveTasks.kt
@@ -1,5 +1,6 @@
 package com.example.xbcad7319
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -21,7 +22,7 @@ class ActiveTasks : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_active_tasks)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activeTasks)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
